e600cf122ca8ff2028ed299a438f0575bcf9026b
Luisa Velandia <luisa.velandia8@gmail.com>
2025-02-22 09:34:04 -0600

    update timer for maze game

--- Minigames/MazeMinigame/flag.gd
+++ Minigames/MazeMinigame/flag.gd
@@ -1,24 +1,66 @@
 extends Area2D
 
+var won = false 
+signal won_changed(is_won)
+var timer
+var countdown_label
+var countdown_time = 18.0
 #@export var popup_scene: PackedScene = preload("res://popup.tscn")  # Ensure this is the correct path
-var won = false
 
+var code = "codept1\n"
 func _ready():
+	timer = $Timer
+	countdown_label = $TimerDisplay
+	print(countdown_label)
 	connect("body_entered", _on_body_entered)
-
+	countdown_label.text = str("0:"+ str(countdown_time))
+	timer.wait_time = 1.0  # Update every second
+	timer.start()  # Start the timer
+	timer.one_shot = false
+	
 func _on_body_entered(body):
 	print("Something entered:", body.name)
 	if body.is_in_group("player"):  # Check if the player entered
 		print("Player entered trigger!")
 		won = true
-		var player = get_node("/root/main_world/player")
-		player.set_movement(true)
-		
+		emit_signal("won_changed", won)
+		save_to_file(code)		
 		close_popup()
 		
 
 func close_popup():
+	var player = get_node("/root/main_world/player")
+	player.set_movement(true)
 	var popup = get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().get_parent()  # Traverse up to Popup
 	if popup:  # Ensure it's a Popup
 		popup.hide()  # Hide the popup
 		popup.queue_free()  # Optionally remove it
+
+func _process(delta):
+	countdown_timer(delta)
+
+func save_to_file(content):
+	var path = "user://pink.txt"
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_string(content)
+	var absolute_path = ProjectSettings.globalize_path(path)
+	OS.shell_open(absolute_path)
+	
+func countdown_timer(delta):
+	var player = get_node("../../MazePlayer")
+	
+	if countdown_time > 0 && !won:
+		countdown_time -= delta 
+		if countdown_time >=10:
+			countdown_label.text = str("0:" + str(int(countdown_time)))
+		else:
+			countdown_label.text = str("0:0" + str(int(countdown_time)))
+	elif countdown_time <=0 && !won:
+		player.set_movement(false)
+		countdown_label.text = str("You lose!")
+		await get_tree().create_timer(2).timeout
+		close_popup()
+	else:	
+		countdown_time = 0 
+		countdown_label.text = str("Time's up!")
+		timer.stop()
