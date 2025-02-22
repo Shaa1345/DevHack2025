7c91c20b902945dc031a3af8c986f3ac6cbeb8aa
Matthew A. <robert_andico@outlook.com>
2025-02-22 12:55:12 -0600

    finalized

--- scene/typing_test_control.gd
+++ scene/typing_test_control.gd
@@ -3,15 +3,15 @@ extends Control
 var word_label : RichTextLabel
 var input_field : LineEdit
 
-var paragraph : String = "import os\nimport time\nimport random\nimport logging\nlogging.basicConfig(filename='system_log.txt', level=logging.INFO)\ndef activate_firewall():\n    print(\"Activating firewall...\")\n    time.sleep(1)\n    logging.info(\"Firewall activated.\")\n    print(\"Firewall enabled.\\n\")\ndef run_antivirus_scan():\n    print(\"Scanning for malware...\")\n    time.sleep(2)\n    if random.choice([True, False]):\n        print(\"Malware detected! Removing...\")\n        logging.info(\"Malware removed.\")\n        return True\n    else:\n        print(\"No malware detected.\")\n        return False\ndef check_for_processes():\n    print(\"Checking processes...\")\n    suspicious_processes = [\"hacker_shell\", \"malicious_script\"]\n    found = False\n    for process in suspicious_processes:\n        if random.choice([True, False]):\n            print(f\"Suspicious process {process} found!\")\n            logging.warning(f\"Process {process} terminated.\")\n            found = True\n    return found\ndef secure_files():\n    print(\"Securing sensitive files...\")\n    time.sleep(1)\n    files = [\"user_data.txt\", \"private_docs.pdf\"]\n    for file in files:\n        logging.info(f\"Secured {file}.\")\n    print(\"Files secured.\\n\")\ndef block_hacker():\n    print(\"Blocking hacker...\")\n    activate_firewall()\n    run_antivirus_scan()\n    check_for_processes()\n    secure_files()\n    print(\"Hacker blocked!\\n\")\nif __name__ == \"__main__\":\n    block_hacker()"
+var paragraph : String = "import random, logging\nlogging.basicConfig(filename='system_log.txt', level=logging.INFO)\ndef block_hacker():\n>>>> print(\"Firewall activated.\\n\")\n>>>> logging.info(\"Firewall enabled.\")\n>>>> if random.choice([True, False]):\n>>>> >>>> print(\"Malware removed.\\n\")\n>>>> >>>> logging.info(\"Malware removed.\")\n>>>> if random.choice([True, False]):\n>>>> >>>> process = random.choice([\"hacker_shell\", \"malicious_script\"])\n>>>> >>>> print(f\"Terminated {process}.\\n\")\n>>>> >>>> logging.warning(f\"Process {process} terminated.\")\n>>>> print(\"Files secured.\\n\")\n>>>> logging.info(\"Sensitive files secured.\")\n>>>> print(\"Hacker blocked!\\n\")\nif __name__ == \"__main__\":\n>>>> block_hacker()\n"
 var lines : Array = []
 var current_line_index : int = 0
 var current_word_index : int = 0
 var current_word : String = ""
 
 func _ready():
-	word_label = $WordDisplay
-	input_field = $UserInput
+	word_label = $ColorRect/WordDisplay
+	input_field = $ColorRect/UserInput
 
 	lines = paragraph.split("\n")
 
@@ -32,9 +32,9 @@ func update_word_display():
 			var line_display = ""
 			for word_index in range(words_in_line.size()):
 				if line_index < current_line_index or (line_index == current_line_index and word_index < current_word_index):
-					line_display += "[color=green]" + words_in_line[word_index] + "[/color] "
+					line_display += "[color=pink]" + words_in_line[word_index] + "[/color] "
 				elif line_index == current_line_index and word_index == current_word_index:
-					line_display += "[color=white]" + words_in_line[word_index] + "[/color] "
+					line_display += "[color=white][b][i]" + words_in_line[word_index] + "[/i][/b][/color] "
 				else:
 					line_display += words_in_line[word_index] + " "
 			
@@ -43,21 +43,23 @@ func update_word_display():
 	word_label.text = display_text.strip_edges()
 
 func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		input_field.text = input_field.text.lstrip(" ")
+	if event.is_action_pressed("ui_accept") && input_field.caret_column == input_field.text.length():
+		input_field.text = input_field.text.strip_edges()
 		if input_field.text == lines[current_line_index].split(" ")[current_word_index]:
+			print((lines[current_line_index].split(" ")[current_word_index]))
 			current_word_index += 1
+			input_field.clear()
 		else:
-			pass
+				var allText = input_field.text.strip_edges().split(" ")
+				input_field.text = input_field.text.strip_edges()
 		
-		input_field.clear()
 
 		if current_word_index >= lines[current_line_index].split(" ").size():
 			current_line_index += 1
 			current_word_index = 0
 
 			if current_line_index >= lines.size():
-				input_field.hide()
+				get_tree().change_scene_to_file("res://scene/goodEnding.tscn")
 			else:
 				current_word = lines[current_line_index].split(" ")[current_word_index]
 		
--- script/boss_fight/boss_background.gd
+++ script/boss_fight/boss_background.gd
@@ -2,7 +2,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	$BossTimer.start(180)
+	$timeDisp/BossTimer.start(480)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +15,7 @@ func game_over() -> void:
 	$AnimationPlayer.advance(180)
 	$AnimationPlayer/RichTextLabel.size=Vector2($AnimationPlayer/RichTextLabel.size.x, 9)
 	$Taskbar.hide()
+	$WindowClosingMinigame/Camera2D/windows.hide()
+	$TypingTest/ColorRect.hide()
+	$timeDisp.hide()
+	
