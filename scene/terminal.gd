0a630591a473e9be64929c70ea93c6210268e434
Matthew Andico <143476442+robertmatthew-a@users.noreply.github.com>
2025-03-06 21:55:49 -0600

    Clean up game

--- scene/terminal.gd
+++ scene/terminal.gd
@@ -14,5 +14,8 @@ func _on_body_entered(body):
 		player.set_movement(false)
 
 func open_popup():
+	call_deferred("_pop_up")
+	
+func _pop_up():
 	var popup = popup_scene.instantiate()
 	get_tree().current_scene.add_child(popup)  # Show the popup in the game
